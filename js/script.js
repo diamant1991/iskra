b5363c68328fe6c894bc2814b165da53038b113d
diamant1991 <diamant1988@mail.ru>
2015-04-02 17:54:30 +0300

    pravka

--- js/script.js
+++ js/script.js
@@ -2,12 +2,10 @@ $(document).ready(function () {
 
 $('.with-drop').hover(function(){
         $(".main-product-container").slideDown(300);
-        $(this).css("text-decoration","underline");
        
       },
       function(){
         $(".main-product-container").slideUp(300);
-        $(this).css("text-decoration","none");
         
       });
 });
