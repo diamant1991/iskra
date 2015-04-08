a46eddab6f3becf076a78090e1bebb6e53f7d79a
diamant1991 <diamant1988@mail.ru>
2015-04-08 15:28:52 +0300

    pravki

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
 
 $('.with-drop').hover(function(){
         $(".main-product-container").slideDown(450);
+        $(".main-product-container").css("visibility","visible");
+        $(".main-product-container").css("display","block")
        
       },
       function(){
-        $(".main-product-container").slideUp(300);
-        
+        $(".main-product-container").css("display","none");
+        $(".main-product-container").css("visibility","hidden");
       });
 });
